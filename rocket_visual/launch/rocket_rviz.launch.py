99509828d5d5379f9a843c1c2e947fc11d80751d
FranzPaj <francescopajero@gmail.com>
2023-05-30 12:01:42 +0200

    Rviz urdf model

--- rocket_visual/launch/rocket_rviz.launch.py
+++ rocket_visual/launch/rocket_rviz.launch.py
@@ -1,17 +1,46 @@
+import os
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
+
+    pkg_path = get_package_share_directory("rocket_visual")
+
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+
+    urdf_file_name = 'rocket_model.urdf.xml'
+    urdf = os.path.join(
+        pkg_path,
+        urdf_file_name)
+    with open(urdf, 'r') as infp:
+        robot_desc = infp.read()
+
+    rviz_config_file_name = 'rviz_basic.rviz'
+    rviz_config = os.path.join(
+        pkg_path,
+        rviz_config_file_name)
+
     return LaunchDescription([
+        
+    #    Node(
+    #        package='rocket_visual',
+    #        executable='broad_serial',
+    #        name='rocket_broadcaster',
+    #        parameters=[
+    #            {'rocket_frame': 'rocket1'}
+    #        ]
+    #    ),
         Node(
-            package='rocket_visual',
-            executable='broad_serial',
-            name='rocket_broadcaster',
-            parameters=[
-                {'rocket_frame': 'rocket1'}
-            ]
-        ),
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'use_sim_time': False, 'robot_description': robot_desc}],
+            arguments=[urdf]),
         Node(
             package='rocket_visual',
             executable='static_broad',
@@ -21,6 +50,7 @@ def generate_launch_description():
         Node(
             package='rviz2',
             executable='rviz2',
-            name='rviz_rocket'
+            name='rviz_rocket',
+            arguments = ['-d', rviz_config],
         )
     ])
--- rocket_visual/setup.py
+++ rocket_visual/setup.py
@@ -1,5 +1,7 @@
 import os
 from glob import glob
+from setuptools import setup
+from setuptools import find_packages
 
 from setuptools import setup
 
@@ -14,6 +16,8 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        (os.path.join('share', package_name), glob('urdf/*')),
+        (os.path.join('share', package_name), glob('rviz/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
